3f3308c85e4056e700b0a0cfa9b45ea636b87941
nouh <nouh.nowile@gmail.com>
2025-11-20 17:01:30 +0100

    WVW: je vien activer le button du ajouter un member

--- src/add.ts
+++ src/add.ts
@@ -279,6 +279,8 @@ function createStackElement(
   return newEl
 }
 
+
+
 function initStackElements(member: IAMember) {
   const newEl = document.createElement('div')
 
@@ -336,4 +338,272 @@ function initStackElements(member: IAMember) {
 
   return newEl
 }
- 
+
+
+
+// modal
+function initModal() {
+  addBtn.addEventListener("click", () => {
+    modal.classList.remove("is-hidden");
+    removeErrorMsg();
+    previewImage();
+  });
+
+  const closeBtn = document.getElementById("close-modal")!;
+  closeBtn.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  form.reset();
+  removeExpDom()
+  imgPrev.src = "./assets/avatars/favatar.webp";
+  modal.classList.add("is-hidden");
+}
+
+function removeExpDom() {
+  document.querySelectorAll(".experiences .experience").forEach((e) => (e as HTMLElement).outerHTML = "");
+}
+
+function extractExper(): IExperience[] | null {
+  const allExpItems = document.querySelectorAll<HTMLElement>(".experience");
+  if (allExpItems.length === 0) return [];
+
+  const experiences: IExperience[] = [];
+
+  allExpItems.forEach((e) => {
+    const id = e.id;
+
+    const company = e.querySelector(`#company-${id}`) as HTMLInputElement;
+    const role = e.querySelector(`#role-${id}`) as HTMLInputElement;
+    const from = ((e.querySelector(`#startDate-${id}`) as HTMLInputElement));
+    const endVal = (e.querySelector(`#endDate-${id}`) as HTMLInputElement);
+    const to = endVal && endVal.value ? new Date(endVal.value) : null;
+
+    const inputs = { company, role, from: from, to: endVal };
+
+    if (validateExperHtmlInputs(inputs)) {
+      experiences.push({ id, company: company.value, role: role.value, from: new Date(from.value), to });
+    }
+  });
+
+  return experiences.length === 0 ? null : experiences;
+}
+
+function experienceGene(id: number): HTMLElement {
+  const container = document.createElement("div");
+  container.className = "experience";
+  container.id = `${id}`;
+
+  const head = document.createElement("div");
+  head.className = "exp-head";
+
+  const title = document.createElement("p");
+  title.textContent = `Experience`;
+
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+  delBtn.addEventListener("click", () => container.remove());
+
+  head.appendChild(title);
+  head.appendChild(delBtn);
+
+  const createField = (label: string, id: string, type: string) => {
+    const group = document.createElement("div");
+    group.className = "form__group";
+
+    group.innerHTML = `
+      <label class="form__label" for="${id}">${label}</label>
+      <input id="${id}" class="input" type="${type}" />
+    `;
+
+    return group;
+  };
+
+  container.appendChild(head);
+  container.appendChild(createField("Company", `company-${id}`, "text"));
+  container.appendChild(createField("Role", `role-${id}`, "text"));
+  container.appendChild(createField("Start Date", `startDate-${id}`, "date"));
+  container.appendChild(createField("End Date", `endDate-${id}`, "date"));
+
+  return container;
+}
+
+function initForm() {
+  const expBtn = document.querySelector(".btn-add-exp");
+
+  expBtn?.addEventListener("click", () => {
+    const exp = experienceGene(experienceId++);
+    const container = document.querySelector(".experiences")!;
+    container.insertBefore(exp, expBtn);
+  });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    removeErrorMsg();
+
+    const nameInput = document.getElementById("name") as HTMLInputElement;
+    const emailInput = document.getElementById("email") as HTMLInputElement;
+    const phoneInput = document.getElementById("phone") as HTMLInputElement;
+    const imageInput = document.getElementById("image") as HTMLInputElement;
+    const roleSelect = document.getElementById("role") as HTMLSelectElement;
+
+    const fields = {
+      name: nameInput,
+      email: emailInput,
+      phone: phoneInput,
+      image: imageInput
+    };
+
+    const valid = validateHtmlInputs(fields);
+    const experiences = extractExper();
+
+    if (valid && experiences !== null) {
+      const member: IMember = {
+        id: Math.random(),
+        name: nameInput.value,
+        role: toRoleEnumValue(roleSelect.value)!,
+        email: emailInput.value,
+        phone: phoneInput.value,
+        image: imageInput.value,
+        zone: "",
+        experience: experiences
+      };
+
+      document.querySelector("#member-list p")?.classList.add("is-hidden")
+      unassignedMembers.push(member);
+      saveInlocalStorage();
+      renderSideBar(member);
+      closeModal();
+    }
+  });
+}
+
+function createExperienceItem(index: string, experience: IExperience) {
+  const wrapper = document.createElement("div");
+  wrapper.className = "experience";
+  wrapper.id = index;
+  wrapper.setAttribute("data-exp-id", String(index));
+
+  const head = document.createElement("div");
+  head.className = "exp-head";
+
+  const title = document.createElement("p");
+  title.textContent = "Experience";
+
+  head.appendChild(title);
+  wrapper.appendChild(head);
+
+  const createInputGroup = (labelTxt: string, idBase: string, type: string, value: string | Date = "") => {
+    const group = document.createElement("div");
+    group.className = "form__group";
+
+    const label = document.createElement("label");
+    label.className = "form__label";
+    label.setAttribute("for", `${idBase}-${index}`);
+    label.textContent = labelTxt;
+
+    const input = document.createElement("input");
+    input.className = "input";
+    input.id = `${idBase}-${index}`;
+    input.type = type;
+
+    if (type == "date") {
+      const myDate = new Date(value);
+      try {
+        const ymd = myDate.toISOString().split("T")[0].trim();
+        input.value = ymd;
+      } catch (error) {
+
+      }
+    } else {
+      input.value = value as string;
+    }
+
+    group.appendChild(label);
+    group.appendChild(input);
+    return group;
+  };
+
+  wrapper.appendChild(
+    createInputGroup("Company", "company", "text", experience?.company || "")
+  );
+  wrapper.appendChild(
+    createInputGroup("Role", "role", "text", experience?.role || "")
+  );
+  wrapper.appendChild(
+    createInputGroup("Start Date", "startDate", "date", experience.from)
+  );
+  wrapper.appendChild(
+    createInputGroup("End Date", "endDate", "date", experience?.to || "")
+  );
+
+  return wrapper;
+}
+
+
+function previewImage() {
+  const imageInput = document.getElementById("image") as HTMLInputElement;
+
+  imageInput.addEventListener("change", () => {
+    imgPrev.src = imageInput.value
+  });
+}
+
+function validateHtmlInputs(inputs: { [k: string]: HTMLInputElement }) {
+  let ok = true;
+
+  Object.entries(inputs).forEach(([key, input]) => {
+    if (!stringValidate(input.value.trim(), key as inputFields)) {
+      addErrorMessage(input, `Please enter a valid ${key}`, key);
+      ok = false;
+    }
+  });
+
+  return ok;
+}
+
+function validateExperHtmlInputs(inputs: { [k: string]: HTMLInputElement }) {
+  let ok = true;
+
+  if (!stringValidate(inputs.company.value, "name")) {
+    addErrorMessage(inputs.company, "Please enter a valid company", "company");
+    ok = false;
+  }
+  if (!stringValidate(inputs.role.value.trim(), "name")) {
+    addErrorMessage(inputs.role, "Please enter a valid role", "role");
+    ok = false;
+  }
+
+  if (!inputs.from.value.trim()) {
+    addErrorMessage(inputs.from, "Please enter a valid start date", "startDate");
+    ok = false;
+  } else if (new Date(inputs.from.value) > new Date(Date.now())) {
+    addErrorMessage(inputs.from, "Please enter a valid range date", "startDate");
+    ok = false;
+  }
+
+  if (inputs.to && inputs.to.value && new Date(inputs.from.value) > new Date(inputs.to.value)) {
+    addErrorMessage(inputs.to, "Please end should be great than start date", "endDate");
+    ok = false;
+  }
+
+  return ok;
+}
+
+function addErrorMessage(el: HTMLElement, msg: string, cls: string) {
+  removeSpecificError(el.parentElement!);
+
+  const d = document.createElement("div");
+  d.className = `error-msg ${cls}`;
+  d.textContent = msg;
+
+  el.parentElement!.appendChild(d);
+}
+
+function removeSpecificError(parent: HTMLElement) {
+  parent.querySelector(".error-msg")?.remove();
+}
+
+function removeErrorMsg() {
+  document.querySelectorAll(".error-msg").forEach((e) => e.remove());
+}
