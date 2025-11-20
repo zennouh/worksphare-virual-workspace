1ac80e0ec09e43917ac09f6aa30d50eee47e1779
nouh <nouh.nowile@gmail.com>
2025-11-20 15:58:28 +0100

    WVW: je prepare le structure du projet, le html et css

---
+++ scripts/jira.js
@@ -0,0 +1,27 @@
+
+import fs  from "fs/promises";
+
+async function checkMessage() {
+    try {
+        const file = process.argv[2]
+
+        const msg = await fs.readFile(file, "utf-8");
+
+        const regex = /^WVW:\s.+$/
+
+        if (regex.test(msg.trim())) {
+            process.exit(0)
+        } else {
+            console.log("Your message pattren is wrong");
+            process.exit(1)
+        }
+
+    } catch (error) {
+        console.log("You have an issue: ", error.message);
+        process.exit(1)
+
+    }
+
+}
+
+checkMessage()
